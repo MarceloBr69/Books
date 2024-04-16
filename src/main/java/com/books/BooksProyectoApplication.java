cb9b505c178e21c36648e8108e2e82bd4bd87395
MarceloBr69 <marcelo.burgos200405@gmail.com>
2024-04-15 21:21:57 -0400

    no listo

---
+++ src/main/java/com/books/BooksProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksProyectoApplication.class, args);
+	}
+
+}
